d327ef9e16fc95a6b8c89e16c00b25e7a2a354ca
DustinKLo <dustinlo.pcc@gmail.com>
2020-10-23 21:08:45 -0700

    moved engineer man video files to engineer_man directory, fixed README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+h3. Engineer man Youtube series
 https://www.youtube.com/playlist?list=PLlcnQQJK8SUjApd95LIcd3K9XXmE-IeCS
 
---
+++ rust_by_example/01_debug/src/main.rs
@@ -0,0 +1,19 @@
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8,
+}
+
+fn main() {
+    let name = "Peter";
+    let age = 27;
+    let peter = Person {
+        name: name,
+        age: age,
+    };
+    // pretty print
+    println!("{:#?}", peter);
+
+    // regular print
+    println!("{:?}", peter);
+}
